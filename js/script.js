e51f9dc36e17d4070f57907e89aa83f8ecd51735
Steph Laba <steph.laba1@gmail.com>
2016-09-10 18:21:05 -0400

    homepage-skinning add: styles to footer, location and sponsors sections

--- js/script.js
+++ js/script.js
@@ -11,4 +11,27 @@ $(document).on('ready', function() {
      }
   });
  // fixed menu
+
+ var $menuTrigger = $('.menu');
+ var $overlay = $('.nav-overlay');
+ var $close = $('a.close');
+ var $nav = $('.navigation-container .navigation ul');
+ var $body = $('body');
+
+ $menuTrigger.on('click touchstart', function(e){
+ 	e.preventDefault();
+ 	$overlay.removeClass('hide');
+ 	$close.removeClass('hide');
+ 	$nav.addClass('show');
+ 	$body.addClass('no-scroll');
+ });
+
+  $close.on('click touchstart', function(e){
+  	e.preventDefault();
+ 	$overlay.addClass('hide');
+ 	$close.addClass('hide');
+ 	$nav.removeClass('show');
+ 	$body.removeClass('no-scroll');
+ });
+
 });
